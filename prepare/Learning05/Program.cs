8aedecb64df51a5a183fc047e690e107a39c7f33
cambur01 <cambur01@gmail.com>
2024-10-22 19:25:48 -0600

    completed week 5 learning activity

--- prepare/Learning05/Program.cs
+++ prepare/Learning05/Program.cs
@@ -4,6 +4,18 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning05 World!");
+        Assignment assignment1 = new Assignment("Jules", "Fractions");
+        
+        Console.WriteLine($"{assignment1.GetSummary()}\n");
+
+        MathAssignment assignment2 = new MathAssignment("Lucas", "Algebra", "5.2", "6-12");
+        
+        Console.WriteLine($"{assignment2.GetSummary()}");
+        Console.WriteLine($"{assignment2.GetHomeworkList()}\n");
+
+        WritingAssignment assignment3 = new WritingAssignment("Elizabeth", "Exercise Science", "The Effects of Muscular Hypertophy");
+
+        Console.WriteLine($"{assignment3.GetSummary()}");
+        Console.WriteLine($"{assignment3.GetWritingInformation()}\n");
     }
 }
