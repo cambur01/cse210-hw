02e81258b6191fdbd44f9d0383b411c634a799c1
cambur01 <cambur01@gmail.com>
2024-10-26 12:36:37 -0600

    finished shapes assignment for week 6

--- prepare/Learning06/Program.cs
+++ prepare/Learning06/Program.cs
@@ -4,6 +4,27 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning06 World!");
+        List<Shape> shapes = new List<Shape>();
+
+        Square s1 = new Square("blue", 8);
+        shapes.Add(s1);
+
+        Rectangle s2 = new Rectangle("red", 5, 15);
+        shapes.Add(s2);
+
+        Circle s3 = new Circle("green", 4);
+        shapes.Add(s3);
+
+        int shapeNumber = 0;
+
+        foreach (Shape s in shapes)
+        {
+            string color = s.GetColor();
+            double area = s.GetArea();
+            shapeNumber += 1;
+
+            Console.WriteLine($"Shape #{shapeNumber} is {color} and has an area of {area}.");
+        }
+
     }
 }
